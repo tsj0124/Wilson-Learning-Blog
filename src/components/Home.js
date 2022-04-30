b02d5ad5fb88cd54737637948578049860f690c0
tsj0124 <tsj6181@hotmail.com>
2022-04-30 20:10:36 +0800

    Upload learning tutorial wilson blog

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import useFetch from "../custom_hook/useFetch";
+import BlogList from "./BlogList";
+
+const Home = () => {
+	const {
+		data: blogs,
+		isPending,
+		error,
+	} = useFetch("http://localhost:8000/blogs");
+
+	return (
+		<div className="home">
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading... </div>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs" />}
+		</div>
+	);
+};
+
+export default Home;
